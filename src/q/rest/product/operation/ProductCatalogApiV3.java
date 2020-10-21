eb2fbb10beec2aae30ff4888aa8b68452e22c1b9
fareed <fareed@qetaa.com>
2020-10-21 12:47:48 +0300

    added jwt security back, added save search with selection

--- src/q/rest/product/operation/ProductCatalogApiV3.java
+++ src/q/rest/product/operation/ProductCatalogApiV3.java
@@ -31,134 +31,145 @@ public class ProductCatalogApiV3 {
     private AsyncProductApi async;
 
 
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("catalogs")
     @GET
-    public Response getCatalogs(){
+    public Response getCatalogs() {
         Response r = this.getCatalogSecuredRequest(AppConstants.GET_CATALOGS);
-        if(r.getStatus() == 200){
-            List<Catalogs> catalogs = r.readEntity(new GenericType<List<Catalogs>>(){});
+        if (r.getStatus() == 200) {
+            List<Catalogs> catalogs = r.readEntity(new GenericType<List<Catalogs>>() {
+            });
             return Response.ok().entity(catalogs).build();
         }
         return Response.status(400).build();
     }
 
 
-
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("models")
     @GET
-    public Response getModels(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info){
+    public Response getModels(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info) {
         String catalogId = info.getQueryParameters().getFirst("catalogid");
         Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogModels(catalogId));
-        if(r.getStatus() == 200){
-            List<CatalogModel> models = r.readEntity(new GenericType<List<CatalogModel>>(){});
+        if (r.getStatus() == 200) {
+            List<CatalogModel> models = r.readEntity(new GenericType<List<CatalogModel>>() {
+            });
             return Response.ok().entity(models).build();
         }
         return Response.status(400).build();
     }
 
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("cars-by-model")
     @GET
-    public Response getCarsByModel(@Context UriInfo info){
+    public Response getCarsByModel(@Context UriInfo info) {
         String catalogId = info.getQueryParameters().getFirst("catalogid");
         String modelId = info.getQueryParameters().getFirst("modelid");
         String params = info.getQueryParameters().getFirst("params");
         Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogCarsByModel(catalogId, modelId, params));
-        if(r.getStatus() == 200){
+        if (r.getStatus() == 200) {
             return Response.ok().entity(r.readEntity(Object.class)).build();
         }
-        Map<String,Object> map = r.readEntity(Map.class);
+        Map<String, Object> map = r.readEntity(Map.class);
         map.putIfAbsent("requested", AppConstants.getCatalogCarsByModel(catalogId, modelId, params));
         return Response.status(r.getStatus()).entity(map).build();
     }
 
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("car-filters-by-model")
     @GET
-    public Response getCarFiltersByModel(@Context UriInfo info){
+    public Response getCarFiltersByModel(@Context UriInfo info) {
         String catalogId = info.getQueryParameters().getFirst("catalogid");
         String modelId = info.getQueryParameters().getFirst("modelid");
         String params = info.getQueryParameters().getFirst("params");
         Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogCarFiltersByModel(catalogId, modelId, params));
-        if(r.getStatus() == 200){
+        if (r.getStatus() == 200) {
             return Response.ok().entity(r.readEntity(Object.class)).build();
         }
-        Map<String,Object> map = r.readEntity(Map.class);
-        map.putIfAbsent("requested", AppConstants.getCatalogCarFiltersByModel(catalogId, modelId,params));
+        Map<String, Object> map = r.readEntity(Map.class);
+        map.putIfAbsent("requested", AppConstants.getCatalogCarFiltersByModel(catalogId, modelId, params));
         return Response.status(r.getStatus()).entity(map).build();
     }
 
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("cars/info")
     @GET
-    public Response getCarInfo(@Context UriInfo info){
+    public Response getCarInfo(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info) {
         String query = info.getQueryParameters().getFirst("query");
         Response r = this.getCatalogSecuredRequest(AppConstants.getCarInfo(query));
-        if(r.getStatus() == 200){
-            return Response.ok().entity(r.readEntity(Object.class)).build();
+        if (r.getStatus() != 200) {
+            async.saveVinSearch(query, null, header, false);
+            return Response.status(404).build();
         }
-        Map<String,Object> map = r.readEntity(Map.class);
-        map.putIfAbsent("requested", AppConstants.getCarInfo(query));
-        return Response.status(r.getStatus()).entity(map).build();
+        List<CatalogCar> catalogCars = r.readEntity(new GenericType<List<CatalogCar>>() {});
+        if (catalogCars.isEmpty()) {
+            async.saveVinSearch(query, null, header, false);
+        } else {
+            String catalogId = null;
+            if(catalogCars.size() == 1) catalogId = catalogCars.get(0).getCatalogId();
+            async.saveVinSearch(query, catalogId, header, true);
+        }
+        return Response.ok().entity(catalogCars).build();
     }
 
-
-//    @SubscriberJwt
+    @SubscriberJwt
     @Path("cars")
     @GET
-    public Response searchVin(@HeaderParam (HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info) {
-        try{
+    public Response searchVin(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info) {
+        try {
             String vin = info.getQueryParameters().getFirst("vin");
             String catalogId = info.getQueryParameters().getFirst("catalogid");
             Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogCarsByVin(catalogId, vin));
-            if(r.getStatus() != 200){
+            if (r.getStatus() != 200) {
                 async.saveVinSearch(vin, catalogId, header, false);
                 return Response.status(404).build();
             }
-            List<CatalogCar> catalogCars = r.readEntity(new GenericType<List<CatalogCar>>(){});
-            if(catalogCars.isEmpty()){
+            List<CatalogCar> catalogCars = r.readEntity(new GenericType<List<CatalogCar>>() {
+            });
+            if (catalogCars.isEmpty()) {
                 async.saveVinSearch(vin, catalogId, header, false);
-            }else{
+            } else {
                 async.saveVinSearch(vin, catalogId, header, true);
             }
             return Response.status(200).entity(catalogCars).build();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             return Response.status(500).build();
         }
     }
 
 
-//    @SubscriberJwt
+
+
+    @SubscriberJwt
     @Path("groups")
     @GET
-    public Response searchGroups(@Context UriInfo info){
-        try{
+    public Response searchGroups(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @Context UriInfo info) {
+        try {
             String catalogId = info.getQueryParameters().getFirst("catalogid");
             String groupId = info.getQueryParameters().getFirst("groupid");
             String criteria = info.getQueryParameters().getFirst("criteria");
             String carId = info.getQueryParameters().getFirst("carid");
-            if(groupId == null || groupId.equals("")) groupId = null;
+            if (groupId == null || groupId.equals("")) groupId = null;
             String encodedCriteria = (criteria == null || criteria.equals("")) ? null : Helper.getEncodedUrl(criteria);
-            Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogGroups(catalogId, carId, groupId, encodedCriteria));
-            if(r.getStatus() != 200){
-                return Response.status(404).build();
-            }
-            List<CatalogGroup> catalogGroups = r.readEntity(new GenericType<List<CatalogGroup>>(){});
-            catalogGroups.forEach(cg -> {
-                cg.setImg(AppConstants.getImageReplacedLink(cg.getImg()));
-            });
-            return Response.status(200).entity(catalogGroups).build();
 
-        }catch (Exception e){
+            Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogGroups(catalogId, carId, groupId, encodedCriteria));
+            if (r.getStatus() != 200)
+                return Response.status(404).build();
+
+            List<CatalogGroup> catalogGroups = r.readEntity(new GenericType<List<CatalogGroup>>() {});
+            catalogGroups.forEach(cg -> {cg.setImg(AppConstants.getImageReplacedLink(cg.getImg()));});
+
+            if(groupId == null)
+                async.saveVinSearch(null, catalogId, header, true);
+            return Response.status(200).entity(catalogGroups).build();
+        } catch (Exception e) {
             e.printStackTrace();
             return Response.status(500).build();
         }
     }
 
-//    @SubscriberJwt
+    //    @SubscriberJwt
     @Path("parts")
     @GET
     public Response searchParts(@Context UriInfo info) {
@@ -169,62 +180,62 @@ public class ProductCatalogApiV3 {
             String carId = info.getQueryParameters().getFirst("carid");
             String encodedCriteria = (criteria == null || criteria.equals("")) ? null : Helper.getEncodedUrl(criteria);
             Response r = this.getCatalogSecuredRequest(AppConstants.getCatalogParts(catalogId, carId, groupId, encodedCriteria));
-            if(r.getStatus() != 200){
+            if (r.getStatus() != 200) {
                 return Response.status(404).build();
             }
             CatalogPart catalogPart = r.readEntity(CatalogPart.class);
             catalogPart.setImg(AppConstants.getImageReplacedLink(catalogPart.getImg()));
             initProductHolders(catalogPart);
             return Response.status(200).entity(catalogPart).build();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return Response.status(500).build();
         }
     }
 
 
-    private void initProductHolders(CatalogPart catalogPart){
-        for(CatalogPartsGroup cpg : catalogPart.getPartGroups()){
-            for(CatalogPartsList parts : cpg.getParts()){
+    private void initProductHolders(CatalogPart catalogPart) {
+        for (CatalogPartsGroup cpg : catalogPart.getPartGroups()) {
+            for (CatalogPartsList parts : cpg.getParts()) {
                 parts.setProducts(getProducts(parts.getNumber()));
             }
         }
     }
 
-    private List<PublicProduct> getProducts(String partNumber){
+    private List<PublicProduct> getProducts(String partNumber) {
         String jpql = "select b from PublicProduct b where b.productNumber = :value0 and b.status =:value1";
         List<PublicProduct> products = dao.getJPQLParams(PublicProduct.class, jpql, Helper.undecorate(partNumber), 'A');
-        if(products != null) {
+        if (products != null) {
             products.forEach(this::initPublicProduct);
         }
         return products;
     }
 
-    private void initPublicProduct(PublicProduct product){
+    private void initPublicProduct(PublicProduct product) {
         try {
             product.setSalesPrice(getAveragedSalesPrice(product.getId()));
             product.initImageLink();
             product.getBrand().initImageLink();
             product.setVariants(new ArrayList<>());
-        }catch(Exception ex){
+        } catch (Exception ex) {
             product = null;
         }
     }
 
-    private double getAveragedSalesPrice(long productId){
+    private double getAveragedSalesPrice(long productId) {
         String sql = "select avg(b.price + (b.price * b.salesPercentage)) from ProductPrice b where b.productId = :value0 and b.status = :value1";
-        Number n = dao.findJPQLParams(Number.class, sql , productId, 'A');
+        Number n = dao.findJPQLParams(Number.class, sql, productId, 'A');
         return n.doubleValue();
     }
 
 
-    private void vinNotFound(String vin, String catalogId){
-        try{
+    private void vinNotFound(String vin, String catalogId) {
+        try {
             VinNotFound vinNotFound = new VinNotFound();
             vinNotFound.setCatId(catalogId);
             vinNotFound.setVin(vin);
             vinNotFound.setCreated(new Date());
             dao.persist(vinNotFound);
-        }catch (Exception ignore){
+        } catch (Exception ignore) {
             System.out.println("an exception occured!!!");
         }
     }
