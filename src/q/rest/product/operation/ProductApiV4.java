3c3a4ec7e00d7664ede08b6ff21d2158cf68893f
fareed <fareed@qetaa.com>
2020-12-01 20:45:01 +0300

    add search parts to api v4

--- src/q/rest/product/operation/ProductApiV4.java
+++ src/q/rest/product/operation/ProductApiV4.java
@@ -6,6 +6,9 @@ import q.rest.product.dao.DAO;
 import q.rest.product.filter.annotation.SubscriberJwt;
 import q.rest.product.helper.Helper;
 import q.rest.product.helper.KeyConstant;
+import q.rest.product.model.contract.v3.product.PbProduct;
+import q.rest.product.model.entity.v3.product.Product;
+import q.rest.product.model.entity.v3.product.Spec;
 import q.rest.product.model.entity.v4.pblic.PbCompanyProduct;
 import q.rest.product.model.search.SearchObject;
 
@@ -31,6 +34,25 @@ public class ProductApiV4 {
     private AsyncProductApi async;
 
 
+    @SubscriberJwt
+    @POST
+    @Path("search-products")
+    public Response searchProducts(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, SearchObject searchObject){
+        if(searchObject.getQuery() == null || searchObject.getQuery().length() == 0){
+            return Response.status(404).build();
+        }
+        String partNumber = "%" + Helper.undecorate(searchObject.getQuery()) + "%";
+        String jpql = "select b from Product b where b.productNumber like :value0 and b.status =:value1";
+        List<Product> products = dao.getJPQLParams(Product.class, jpql, partNumber, 'A');
+        List<Spec> specs = dao.get(Spec.class);
+        List<PbProduct> pbProducts = new ArrayList<>();
+        for (var product : products) {
+            pbProducts.add(product.getPublicProduct(specs));
+        }
+        return Response.status(200).entity(pbProducts).build();
+    }
+
+
     //for lazy loading
     @SubscriberJwt
     @POST
@@ -43,7 +65,7 @@ public class ProductApiV4 {
         var companyProducts = searchCompanyProducts(searchObject);
         async.saveSearch2(header, searchObject, size > 0);
         Map<String,Object> map = new HashMap<>();
-        map.put("search-size", size);
+        map.put("searchSize", size);
         map.put("companyProducts", companyProducts);
         return Response.status(200).entity(map).build();
     }
