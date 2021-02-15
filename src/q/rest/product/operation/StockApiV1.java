d80a98eeb89bc63dd32240d005339482dbab9929
fareed <fareed@qetaa.com>
2021-02-15 04:02:15 +0300

    edited daily sales, removed branch query param

--- src/q/rest/product/operation/StockApiV1.java
+++ src/q/rest/product/operation/StockApiV1.java
@@ -460,19 +460,14 @@ public class StockApiV1 {
     @SubscriberJwt
     @GET
     @Path("daily-sales/from/{from}/to/{to}")
-    public Response getDailySales(@Context UriInfo info, @HeaderParam(HttpHeaders.AUTHORIZATION) String header, @PathParam(value = "from") long fromLong, @PathParam(value = "to") long toLong){
-        String branchId = info.getQueryParameters().getFirst("branch");
-        String branchSql = "";
-        if(branchId != null){
-            branchSql = " and s.branch_id = " + branchId + " ";
-        }
+    public Response getDailySales(@HeaderParam(HttpHeaders.AUTHORIZATION) String header, @PathParam(value = "from") long fromLong, @PathParam(value = "to") long toLong){
         Helper h = new Helper();
         int companyId = Helper.getCompanyFromJWT(header);
         List<Date> dates = h.getAllDatesBetween2(new Date(fromLong), new Date(toLong));
         List<Map> dailySales = new ArrayList<>();
         for (Date date : dates) {
             String sql = "select sum((i.unit_price * i.quantity + s.delivery_charge) + (i.unit_price * i.quantity + s.delivery_charge) * s.tax_rate) as total from prd_stk_sales_order_item i join prd_stk_sales_order s on i.sales_order_id = s.id " +
-                    " where s.company_id = " +companyId + branchSql + 
+                    " where s.company_id = " +companyId +
                     " and cast(s.created as date ) = '" + h.getDateFormat(date, "yyyy-MM-dd") + "'" +
                     " group by cast(s.created as date)";
             Object o = dao.getNativeSingle(sql);
@@ -482,7 +477,7 @@ public class StockApiV1 {
                     "    join prd_stk_sales_return r on sri.sales_return_id = r.id" +
                     "    join prd_stk_sales_order s on r.sales_id = s.id" +
                     "    join prd_stk_sales_order_item si on sri.sales_item_id = si.id" +
-                    " where s.company_id = " + companyId + branchSql +
+                    " where s.company_id = " + companyId + 
                     " and cast(r.created as date ) = '" + h.getDateFormat(date, "yyyy-MM-dd") + "'" +
                     " group by cast(r.created as date)";
             Object o2 = dao.getNativeSingle(sqlReturn);
